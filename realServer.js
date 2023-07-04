c8458777e936c30efd0e0a66ee4ac7b16a61b677
Tanishk-Bohare <72726478+Tanishk-Bohare@users.noreply.github.com>
2023-07-04 11:07:03 +0530

    Update realServer.js
    updating CORS

--- realServer.js
+++ realServer.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const saveLog = require('./saveLog')
+const cors = require('cors')
 
 const app = express()
 
-
+app.use(cors());
 
 app.get("/home", (req,res)=>{
     saveLog.save("hitting home API" + '\n')
@@ -11,11 +12,10 @@ app.get("/home", (req,res)=>{
 })
 
 app.get('/count', (req, res)=>{
-   
     saveLog.save("hitting count API" + '\n')
     res.send("API count")
 })
 
 app.listen(3001, ()=>{
-    console.log("Real server is running at",3001)
+    console.log("Real server is running at ",3001)
 })
